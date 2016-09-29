f3d094b39e633f53c30637cc4bfabce9b855dcf1
dennis.zhao <zhy32008@gmail.com>
2016-09-29 17:23:22 +0800

    router

--- app/heroes.component.ts
+++ app/heroes.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from './hero';
 import { HeroService } from './hero.service';
+import {Router} from "@angular/router";
 
 
 @Component({
-    selector: 'my-app',
+    // moduleId:module.id,
+    selector: 'my-heroes',
     // template: '<h1>{{title}}</h1><h2>{{hero}} details!</h2>' template 会直接加载里面的内容
-    templateUrl: 'app/view/app.html',
-    providers: [HeroService],
+    templateUrl: 'app/view/heroes.html',
+    styleUrls: ['app/css/heroes.css'],
+    // providers: [HeroService],
 
 })
 
 
-export class AppComponent implements OnInit{
-    title = 'Tour of Heroes';
+export class HeroesComponent implements OnInit{
 
     heroes: Hero[];
 
     selectedHero: Hero ;
 
-    constructor(private heroService: HeroService) { }
+    constructor(private heroService: HeroService, private router:Router) {
+
+    }
 
     getHeroes(): void {
 
@@ -36,6 +40,11 @@ export class AppComponent implements OnInit{
     onSelect(hero: Hero):void{
 
         this.selectedHero = hero;
+    }
+
+    gotoDetail(): void {
+
+        this.router.navigate(['/detail',this.selectedHero.id]);
     }
 }
 
